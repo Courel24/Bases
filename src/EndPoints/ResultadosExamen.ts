250286f600cf47380cbd1119eb224ea202282133
Juan David Cabrera Duran? <juandavid.juandis@gmail.com>
2021-05-07 19:09:44 -0500

    added query

---
+++ src/EndPoints/ResultadosExamen.ts
@@ -0,0 +1,28 @@
+import {getConnection} from "../server";
+
+export const GetResultadosPaciente = async (req: any) => {
+    try{
+        const databaseConnection = getConnection();
+        const {idExamen, ccPaciente} = req.query;
+        if (idExamen){
+            const databaseResponse  = await databaseConnection.query(`SELECT idExamen, ccPaciente, resultado FROM examen WHERE idExamen = ${idExamen}`);
+            return {
+                data: databaseResponse.rows
+            }
+        }
+
+        if (ccPaciente){
+            const databaseResponse  = await databaseConnection.query(`SELECT idExamen, ccPaciente, resultado FROM examen WHERE ccPaciente = ${ccPaciente}`);
+            return {
+                data: databaseResponse.rows
+            }
+        }
+
+        throw new Error("Error getting results");
+
+    }catch (err){
+        return {
+            error: err.message
+        }
+    }
+}
