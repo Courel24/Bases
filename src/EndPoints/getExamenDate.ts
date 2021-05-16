66890ca70db614aa6e23c597c6a33f4699f5a6bb
Courel24 <susanaperez1993@hotmail.com>
2021-05-16 14:27:25 -0500

    DateExam

--- src/EndPoints/getExamenDate.ts
+++ src/EndPoints/getExamenDate.ts
@@ -7,7 +7,7 @@ export const GetExamenDate = async (req: any) => {
     const databaseResponse  = await databaseConnection.query(`select idexamen, ccpaciente as CedulaPaciente,paciente.nombre as NombrePaciente, personal.nombre as NombrePersonal, tipoexamen.nombre as nombreExamen, fechatoma, resultado
     FROM examen INNER JOIN paciente ON (examen.ccpaciente=paciente.cc)
     INNER JOIN personal ON (examen.ccpersonal=personal.cc)
-    INNER JOIN tipoexamen ON (examen.idtipoexamen=tipoexamen.idtipoexamen) WHERE fechatoma =  CAST('${fechatoma}' as DATE) ORDER BY fechatoma DESC;`)
+    INNER JOIN tipoexamen ON (examen.idtipoexamen=tipoexamen.idtipoexamen) WHERE fechatoma =  TO_DATE ('${fechatoma}', 'YYYY-MM-DD') ORDER BY fechatoma DESC;`)
     return {
       data: databaseResponse.rows
     }
